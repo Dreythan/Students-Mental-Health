2d8e3a2c24e9f52208e872a0f529c823a92e0ae8
Dreythan <dreythan18.o@gmail.com>
2025-08-28 19:50:01 -0500

    Delete sql directory

--- sql/01_dom_vs_inter.sql
+++
@@ -1,5 +0,0 @@
-SELECT inter_dom, ROUND(AVG(todep)::numeric, 2) AS avg_depression
-FROM students
-WHERE todep IS NOT NULL
-GROUP BY inter_dom
-ORDER BY inter_dom;
--- sql/sql/04_toas_vs_todep.sql
+++
@@ -1,4 +0,0 @@
-SELECT toas, ROUND(AVG(todep)::numeric, 2) AS avg_depression
-FROM students
-GROUP BY toas
-ORDER BY toas;
