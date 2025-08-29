614c2d3ca0b0b412bfeabda2354169ce0115461d
Dreythan <dreythan18.o@gmail.com>
2025-08-28 20:09:34 -0500

    Delete exports/sql directory

--- exports/sql/02_by_stay.sql
+++
@@ -1,5 +0,0 @@
-SELECT stay, ROUND(AVG(todep)::numeric, 2) AS avg_depression
-FROM students
-WHERE todep IS NOT NULL
-GROUP BY stay
-ORDER BY stay;
--- exports/sql/05_correlations.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  corr(toas, todep) AS r_stress_depression,
-  corr(tosc, todep) AS r_conn_depression
-FROM students
-WHERE todep IS NOT NULL;
